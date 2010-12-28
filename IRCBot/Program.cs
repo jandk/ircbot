3d0cc1f042d5fe294d11cb8295b0849158d5163c
Jan De Kock <jan@tjoener.be>
2010-12-28 16:48:17 +0100

    Added a simple config file.
    Do not commit this config file when creating a new patch.

--- IRCBot/Program.cs
+++ IRCBot/Program.cs
@@ -1,20 +1,49 @@
 
 using System;
+using System.Collections.Specialized;
+using System.IO;
 
 namespace IRCBot
 {
 	class Program
 	{
+		// TODO: Better location
+		static string Config = "config.txt";
+
 		static void Main(string[] args)
 		{
-			using (IRCBot bot = new IRCBot("irc.freenode.net", 6667, "tjoenbot"))
+			var config = ReadConfig();
+
+			using (IRCBot bot = new IRCBot(config["host"], Convert.ToUInt16(config["port"]), config["nick"]))
 			{
-				bot.Join("heleos");
+				bot.Join(config["channel"]);
 
 				Console.WriteLine("Press q to quit...");
 				while (Console.ReadKey(true).KeyChar != 'q')
 					;
 			}
+		}
+
+		static StringDictionary ReadConfig()
+		{
+			if (!Directory.Exists(Config))
+				throw new Exception("Config file not found.");
+
+			StringDictionary config = new StringDictionary();
+
+			string[] lines = File.ReadAllLines(Config);
+			foreach (var line in lines)
+			{
+				if (line.IndexOf('=') <= 0)
+					continue;
+
+				var split = line.Split(new char[] { '=' }, 2);
+
+				if (!config.ContainsKey(split[0].Trim()))
+					config.Add(split[0].Trim(), split[1].Trim());
+			}
+
+			return config;
 		}
 	}
 }
