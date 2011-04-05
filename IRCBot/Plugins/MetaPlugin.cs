2232d7235c9456501f570704cce33451ee273994
Jan De Kock <jan@tjoener.be>
2011-04-06 01:04:05 +0200

    Cleanup usages

--- IRCBot/Plugins/MetaPlugin.cs
+++ IRCBot/Plugins/MetaPlugin.cs
@@ -1,9 +1,9 @@
 
 using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 using IRC;
-using System.Reflection;
-using System.Collections.Generic;
 
 namespace IRCBot.Plugins
 {
