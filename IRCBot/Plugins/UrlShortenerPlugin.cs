49b177981d797e0bb9300935b0c46bbcd195a9ae
Jan De Kock <jan@tjoener.be>
2011-09-08 11:06:18 +0200

    Added a new UrlShortener plugin
    Needs a valid google key in config.txt

---
+++ IRCBot/Plugins/UrlShortenerPlugin.cs
@@ -0,0 +1,86 @@
+
+using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+using IRC;
+using System.Net;
+using System.Text;
+using Parsing.Json;
+using System.IO;
+
+namespace IRCBot.Plugins
+{
+	class UrlShortenerPlugin
+		: IRCPluginBase
+	{
+		const string UrlRegex = @"\b(https?|ftp)://[-A-Za-z0-9+&@#/%?=~_|$!:,.;]*[A-Za-z0-9+&@#/%=~_|$]";
+		static readonly string ApiKey = Config.Instance["url.key"];
+		static readonly Uri ApiUrl = new Uri(@"https://www.googleapis.com/urlshortener/v1/url?key=" + ApiKey);
+
+		protected WebClient poster = new WebClient();
+		protected Scanner scanner = new Scanner();
+		protected Parser parser = new Parser();
+
+		protected override bool Initialize()
+		{
+			Bot.SubscribeToMessage(UrlRegex, ShortenUrl);
+
+			poster.UploadStringCompleted += HandleReply;
+
+			return true;
+		}
+
+		protected void ShortenUrl(IRCMessage message)
+		{
+			Match match = Regex.Match(message.Message, UrlRegex);
+
+			// Skip shortened urls
+			if (match.Value.Contains("goo.gl"))
+				return;
+
+			// Skip short urls
+			if (match.Value.Length <= 20)
+				return;
+
+			// Create new json
+			Hashtable table = new Hashtable();
+			table.Add("longUrl", match.Value);
+			string json = JsonWriter.Write(table);
+
+			Console.WriteLine("Found url: " + match.Value);
+
+			// Post the data to the google url api
+			poster.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+			poster.UploadStringAsync(ApiUrl, null, json, message);
+
+			Console.WriteLine("Waiting for reply...");
+		}
+
+		void HandleReply(object sender, UploadStringCompletedEventArgs e)
+		{
+			if (e.Error != null)
+			{
+				Console.WriteLine("Got status: " + e.Error.ToString());
+				return;
+			}
+
+			Console.WriteLine("Got reply: " + e.Result);
+
+			// Google api always returns an object
+			Hashtable result = (Hashtable)parser.Parse(scanner.Scan(new StringReader(e.Result)));
+
+			string message;
+			if (result.ContainsKey("error"))
+				message = "Error while shortening url.";
+			else if (result.ContainsKey("id"))
+				message = "Short url: " + result["id"];
+			else
+				message = "Unknown error";
+
+			string channel = ((IRCMessage)e.UserState).Channel;
+			Bot.SendChannelMessage(channel, message);
+		}
+
+	}
+}
---
+++ Parsing/Json.cs
@@ -0,0 +1,540 @@
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Parsing.Json
+{
+
+	#region Extensions
+
+	static class Extensions
+	{
+		public static bool IsWhiteSpace(this int c)
+		{
+			switch (c)
+			{
+				case '\t':
+				case '\n':
+				case '\r':
+				case ' ':
+					return true;
+				default:
+					return false;
+			}
+		}
+	}
+
+	#endregion
+
+	#region Kind
+
+	enum Kind
+	{
+		Eof,
+		Colon,
+		Comma,
+		BraceLeft,
+		BraceRight,
+		BracketLeft,
+		BracketRight,
+
+		// Values
+		Number,
+		String,
+		Boolean,
+		Null
+	}
+
+	#endregion
+
+	#region Token
+
+	class Token
+		: IToken
+	{
+		private Kind _kind;
+		private object _value;
+
+
+		public Kind Kind { get { return _kind; } }
+		public object Value { get { return _value; } }
+
+
+		private Token(Kind kind)
+		{
+			_kind = kind;
+		}
+
+		private Token(Kind kind, object value)
+		{
+			_kind = kind;
+			_value = value;
+		}
+
+
+		public bool IsValue
+		{
+			get
+			{
+				switch (_kind)
+				{
+					case Kind.String:
+					case Kind.Number:
+					case Kind.Boolean:
+					case Kind.Null:
+						return true;
+					default:
+						return false;
+				}
+			}
+		}
+
+
+		public override string ToString()
+		{
+			return String.Format(
+				"[Kind: {0}, Value: {1}]",
+				Kind.ToString(),
+				(Value ?? String.Empty).ToString()
+			);
+		}
+
+		static public Token FromKind(Kind kind)
+		{
+			return new Token(kind);
+		}
+
+		static public Token FromString(string value)
+		{
+			return new Token(Kind.String, value);
+		}
+
+		static public Token FromNumber(double value)
+		{
+			return new Token(Kind.Number, value);
+		}
+
+		static public Token FromBoolean(bool value)
+		{
+			return new Token(Kind.Boolean, value);
+		}
+
+		static public Token FromNull()
+		{
+			return new Token(Kind.Null, null);
+		}
+	}
+
+	#endregion
+
+	#region Scanner
+
+	class Scanner
+		: ScannerBase<Token>
+	{
+
+		private static StringBuilder _sb = new StringBuilder(1024);
+
+		#region Strings
+
+		string ScanString()
+		{
+			_sb.Length = 0;
+
+			Read();
+
+			while (true)
+			{
+				int c = Read();
+
+				if (c == '"')
+					break;
+
+				if (c == '\\')
+				{
+					switch (Read())
+					{
+						case '\\': c = '\\'; break;
+						case '"': c = '"'; break;
+						case '/': c = '/'; break;
+						case 'b': c = '\b'; break;
+						case 'f': c = '\f'; break;
+						case 'n': c = '\n'; break;
+						case 'r': c = '\r'; break;
+						case 't': c = '\t'; break;
+						case 'u':
+							c = (char)(
+								Read().FromHex() << 12 |
+								Read().FromHex() << 8 |
+								Read().FromHex() << 4 |
+								Read().FromHex()
+							);
+							break;
+						default:
+							Throw("Invalid escape sequence");
+							break;
+					}
+				}
+
+				_sb.Append((char)c);
+			}
+
+			return _sb.ToString();
+		}
+
+		#endregion
+
+		#region Numbers
+
+		double ScanNumber()
+		{
+			bool negate = Maybe('-');
+
+			if (!Peek().IsDec())
+				Throw("Expected a digit");
+
+			// Integer part
+			if (Maybe('0'))
+				return 0;
+
+			double d = 0;
+			while (Peek().IsDec())
+				d = (d * 10) + Read().FromDec();
+
+			// Fractional part
+			if (Maybe('.'))
+			{
+				double f = 0;
+				double w = 0.1;
+
+				if (!Peek().IsDec())
+					Throw("At least one digit after '.'");
+
+				while (Peek().IsDec())
+				{
+					f += w * Read().FromDec();
+					w *= 0.1;
+				}
+
+				d += f;
+			}
+
+			// Exponent
+			if (Maybe('e') || Maybe('E'))
+			{
+				bool negateExponent = false;
+
+				if (Peek() == '+' || Peek() == '-')
+				{
+					negateExponent = Peek() == '-';
+					Read();
+				}
+
+				if (!Peek().IsDec())
+					Throw("At least one digit after 'e' or 'E'.");
+
+				double e = 0;
+				while (Peek().IsDec())
+					e = (e * 10) + Read().FromDec();
+
+				if (negateExponent)
+					e = -e;
+
+				d *= Math.Pow(10, e);
+			}
+
+			if (negate)
+				d = -d;
+
+			return d;
+		}
+
+		#endregion
+
+		#region IScanner<Token> Members
+
+		protected override IEnumerator<Token> Scan()
+		{
+			while (Peek() != -1)
+			{
+				int peek = Peek();
+
+				if (peek.IsWhiteSpace())
+				{
+					Read();
+					continue;
+				}
+
+				if (peek.IsDec() || peek == '-')
+				{
+					double value = ScanNumber();
+					yield return Token.FromNumber(value);
+					continue;
+				}
+
+				switch (peek)
+				{
+					case '"':
+						string value = ScanString();
+						yield return Token.FromString(value);
+						break;
+
+					case 't':
+						Expect("true");
+						yield return Token.FromBoolean(true);
+						break;
+
+					case 'f':
+						Expect("false");
+						yield return Token.FromBoolean(false);
+						break;
+
+					case 'n':
+						Expect("null");
+						yield return Token.FromNull();
+						break;
+
+					// Operators come last
+					case '{': Read();
+						yield return Token.FromKind(Kind.BraceLeft);
+						break;
+
+					case '}': Read();
+						yield return Token.FromKind(Kind.BraceRight);
+						break;
+
+					case '[': Read();
+						yield return Token.FromKind(Kind.BracketLeft);
+						break;
+
+					case ']': Read();
+						yield return Token.FromKind(Kind.BracketRight);
+						break;
+
+					case ':': Read();
+						yield return Token.FromKind(Kind.Colon);
+						break;
+
+					case ',': Read();
+						yield return Token.FromKind(Kind.Comma);
+						break;
+
+					default:
+						Throw("Invalid character: " + Peek());
+						break;
+				}
+
+			}
+
+			yield return Token.FromKind(Kind.Eof);
+		}
+
+		#endregion
+
+	}
+
+
+	#endregion
+
+	#region Parser
+
+	class Parser
+		: ParserBase<Token>
+	{
+
+		protected override object Parse()
+		{
+			_ts.MoveNext();
+
+			object value = JsonValue();
+
+			_ts.MoveNext();
+			if (_ts.Current.Kind != Kind.Eof)
+				throw new Exception("Expected EOF");
+
+			return value;
+		}
+
+		ArrayList JsonArray()
+		{
+			ArrayList list = new ArrayList();
+
+			_ts.MoveNext();
+			if (_ts.Current.Kind == Kind.BracketRight)
+				return list;
+
+			while (true)
+			{
+				list.Add(JsonValue());
+
+				_ts.MoveNext();
+				switch (_ts.Current.Kind)
+				{
+					case Kind.Comma:
+						_ts.MoveNext();
+						continue;
+					case Kind.BracketRight:
+						return list;
+					default:
+						throw new Exception("Expected 'Colon' or 'BracketRight'.");
+				}
+			}
+		}
+
+		Hashtable JsonObject()
+		{
+			Hashtable dict = new Hashtable();
+
+			_ts.MoveNext();
+			if (_ts.Current.Kind == Kind.BraceRight)
+				return dict;
+
+			while (true)
+			{
+				if (_ts.Current.Kind != Kind.String)
+					throw new Exception("Expected 'String' or 'BraceRight', got " + _ts.Current.Kind.ToString());
+
+				string name = (string)_ts.Current.Value;
+
+				_ts.MoveNext();
+				if (_ts.Current.Kind != Kind.Colon)
+					throw new Exception("Expected 'Colon', got " + _ts.Current.Kind.ToString());
+
+				_ts.MoveNext();
+				dict.Add(name, JsonValue());
+
+				_ts.MoveNext();
+				switch (_ts.Current.Kind)
+				{
+					case Kind.Comma:
+						_ts.MoveNext();
+						continue;
+					case Kind.BraceRight:
+						return dict;
+					default:
+						throw new Exception("Expected a comma or a right brace");
+				}
+			}
+		}
+
+		object JsonValue()
+		{
+			if (_ts.Current.IsValue)
+				return _ts.Current.Value;
+
+			switch (_ts.Current.Kind)
+			{
+				case Kind.BracketLeft:
+					return JsonArray();
+				case Kind.BraceLeft:
+					return JsonObject();
+				default:
+					throw new Exception("Expected 'BracketLeft' or 'BraceLeft', got " + _ts.Current.Kind.ToString());
+			}
+		}
+	}
+
+	#endregion
+
+	#region JsonWriter
+
+	class JsonWriter
+	{
+
+		StringBuilder _sb = new StringBuilder(1024);
+
+
+		public static string Write(object value)
+		{
+			JsonWriter writer = new JsonWriter();
+			writer.Convert(value);
+			return writer._sb.ToString();
+		}
+
+
+		void Convert(object value)
+		{
+			if (value is ArrayList)
+			{
+				ArrayList list = (ArrayList)value;
+
+				_sb.Append('[');
+
+				foreach (var item in list)
+				{
+					Convert(item);
+					_sb.Append(',');
+				}
+
+				if (list.Count > 0)
+					_sb.Length -= 1;
+
+				_sb.Append(']');
+				return;
+			}
+
+			else if (value is Hashtable)
+			{
+				Hashtable hash = (Hashtable)value;
+
+				_sb.Append('{');
+
+				foreach (var item in hash)
+				{
+					DictionaryEntry entry = (DictionaryEntry)item;
+
+					Convert(entry.Key.ToString());
+					_sb.Append(':');
+					Convert(entry.Value);
+					_sb.Append(',');
+				}
+
+				if (hash.Count > 0)
+					_sb.Length -= 1;
+
+				_sb.Append('}');
+			}
+
+			else if (value is string)
+			{
+				string str = (string)value;
+
+				_sb.Append('"');
+				foreach (char c in str)
+				{
+					switch (c)
+					{
+						case '"': _sb.Append("\\\""); break;
+						case '\\': _sb.Append("\\\\"); break;
+						case '/': _sb.Append("\\/"); break;
+						case '\b': _sb.Append("\\b"); break;
+						case '\f': _sb.Append("\\f"); break;
+						case '\n': _sb.Append("\\n"); break;
+						case '\r': _sb.Append("\\r"); break;
+						case '\t': _sb.Append("\\t"); break;
+						default: _sb.Append(c); break;
+					}
+				}
+				_sb.Append('"');
+			}
+
+			else if (value is double || value is int)
+				_sb.Append(((IFormattable)value).ToString("G", NumberFormatInfo.InvariantInfo));
+
+			else if (value is bool)
+				_sb.Append((bool)value ? "true" : "false");
+
+			else if (value == null)
+				_sb.Append("null");
+
+			else throw new Exception("Cannot convert type");
+		}
+
+	}
+
+	#endregion
+
+}
